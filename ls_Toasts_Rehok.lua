726ea6b3fe67c6a5c8166af9e1c8fe29e1e92dc1
Rehok <Rehok@users.noreply.github.com>
2017-08-26 17:59:19 +0100

    Clean Up
    Remove parts of code that are not needed. Added code for the new textures so it now follows whatever Ls: toasts says the colour for the border should be. No longer includes Icon borders as this is part of the toast border to make it look cleaner.
    
    Anything that would have a white border (currency etc) now defaults to black border, You shouldn't see any changes from what it looks like now.

--- ls_Toasts_Rehok.lua
+++ ls_Toasts_Rehok.lua
@@ -1,39 +1,28 @@
-if not IsAddOnLoaded 'ls_Toasts' then 
-	return 
+if not IsAddOnLoaded 'ls_Toasts' then
+	return
 end
 
 local LST = unpack(ls_Toasts)
 
+
 function LST:SkinToast(toast, toastType)
-	toast.Border:SetAlpha(1)
-	toast.Border:SetVertexColor(0,0,0)
+	toast.Border:SetTexture([[Interface\AddOns\ls_Toasts_Rehok\media\toast-Border.tga]])
 	toast.TextBG:SetAlpha(1)
 	toast.BG:SetTexture([[Interface\AddOns\ls_Toasts_Rehok\media\normTex2.tga]]) -- This is from ElvUI
 	toast.BG:SetAlpha(1)
 	toast.BG:SetVertexColor(0.15,0.15,0.15)
-	toast.Border:SetTexture([[Interface\AddOns\ls_Toasts_Rehok\media\toast-border.tga]])
 	toast.Icon:SetTexCoord(.08, .92, .08, .92)
+	toast.IconBorder:SetTexture("")
 	toast.Title:SetFont([[Interface\AddOns\ls_Toasts_Rehok\media\Expressway.ttf]], 16)
 	toast.Title:SetPoint("TOPLEFT", 55, -12)
-	
-	if toast.IconText1 then
-		if #toast.IconText1 <= 1 then
-			toast.IconText1:SetText("")
-		end
-	end
-	if toast.IconBorder then
-		local r, g, b = toast.IconBorder:GetVertexColor()
-		
-	if r > 0.99 and g > 0.99 and b > 0.99 then
-		toast.IconBorder:SetVertexColor(0, 0, 0, 1)
-		toast.IconBorder:SetTexture([[Interface\AddOns\ls_Toasts_Rehok\media\icon-border.tga]])
+
+	local r,g,b = toast.Border:GetVertexColor()
+
+	if (r >= 0.9) and (g >= 0.9) and (b >= 0.9) then
+		toast.Border:SetTexture([[Interface\AddOns\ls_Toasts_Rehok\media\black-border.TGA]])
 	end
 
 	if toast.Dragon then
 		toast.Dragon:SetTexture()
-	end
-
-		toast.IconBorder:SetTexture([[Interface\AddOns\ls_Toasts_Rehok\media\icon-border.tga]])
-		toast.IconBorder:SetTexCoord(0, 1, 0, 1)
 	end
 end
