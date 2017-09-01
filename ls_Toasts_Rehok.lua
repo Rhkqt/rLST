4a12e06b3297576ea066ba6bc3bc39e60c57f4e7
Rehok <Rehok@users.noreply.github.com>
2017-09-01 19:27:48 +0100

    Update ls_Toasts_Rehok.lua
    Fixes texture and makes the Icon fill out the entire square so icon border is no longer required, Bit buggy with Currencies (Order Resources etc, Not sure why?)

--- ls_Toasts_Rehok.lua
+++ ls_Toasts_Rehok.lua
@@ -12,19 +12,29 @@ function LST:SkinToast(toast, toastType)
 	toast.BG:SetAlpha(1)
 	toast.BG:SetVertexColor(0.15,0.15,0.15)
 	toast.Icon:SetTexCoord(.08, .92, .08, .92)
-	toast.IconBorder:SetTexture("")
+	toast.Icon:SetPoint("TOPLEFT", 5, -6)
+	toast.Icon:SetSize(47,47)
+	toast.IconBorder:SetTexture()
+	toast.IconText1BG:SetTexture()
 	toast.Title:SetFont([[Interface\AddOns\ls_Toasts_Rehok\media\Expressway.ttf]], 16)
 	toast.Title:SetPoint("TOPLEFT", 55, -12)
 
 	local r,g,b = toast.Border:GetVertexColor()
 
 	if (r >= 0.9) and (g >= 0.9) and (b >= 0.9) then
-		toast.Border:SetTexture([[Interface\AddOns\ls_Toasts_Rehok\media\black-border.TGA]])
+		toast.Border:SetTexture([[Interface\AddOns\ls_Toasts_Rehok\media\black-Border.TGA]])
+	end
+	if toast.iconHL then
+		return toast.iconHL:SetTexture()
+	end
+	if toast.Shine then
+		toast.Shine:SetTexture("")
+	end
+	if toast.Glow then
+		toast.Glow:SetTexture("")
+	end
+	if toast.Dragon then
+		toast.Dragon:SetTexture("")
 	end
 
-	if toast.Dragon then
-		toast.Dragon:SetTexture()
-	end
-	
-	-- Scale screws up sizing of the border. Find a way to resize the border to keep it matched with the size of the toasts
 end
