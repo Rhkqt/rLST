7e2705b6199f6a46d328d8e1f9fa14bf0271b82f
Rehok <Rehok@users.noreply.github.com>
2018-02-12 18:37:50 +0000

    Pushed update too early

--- ls_Toasts_Rehok/ls_Toasts_Rehok.lua
+++ ls_Toasts_Rehok/ls_Toasts_Rehok.lua
@@ -1,66 +1,45 @@
 if not IsAddOnLoaded 'ls_Toasts' then
 	return
 end
-local LS = unpack(ls_Toasts)
 
-LS:RegisterSkin("rlst", {
-		name = "rLST",
-		template = "elv-no-art",
-		border = {
-			color = {0,0,0},
-			texture = {0, 0, 0, 1},
-		},
-		icon_border = {
-			color = {0,0,0,0},
-		},
-		dragon = {
-		    hidden = true,
-		  },
-		icon_highlight = {
-		    hidden = true,
-		  },
-		bg = {
-				alliance = {0,0,0,0.9},
-				archaeology = {0,0,0, 0.9},
-				collection = {0,0,0,0.9},
-				default = {0,0,0, 0.9},
-				dungeon = {0,0,0, 0.9},
-				horde = {0,0,0, 0.9},
-				legendary = {0,0,0, 0.9},
-				legion = {0,0,0, 0.9},
-				recipe = {0,0,0, 0.9},
-				store = {0,0,0, 0.9},
-				transmog = {0,0,0, 0.9},
-				upgrade = {0,0,0, 0.9},
-				worldquest = {0,0,0, 0.9},
-			},
-})
+local LST = unpack(ls_Toasts)
 
-LS:RegisterSkin("rlst-colour", {
-	name = "rLST (Colour)",
-	template = "elv-no-art",
-	icon_border = {
-		color = {0,0,0,0},
-	},
-	dragon = {
-			hidden = true,
-		},
-	icon_highlight = {
-			hidden = true,
-		},
-	bg = {
-			alliance = {0,0,0,0.9},
-			archaeology = {0,0,0, 0.9},
-			collection = {0,0,0,0.9},
-			default = {0,0,0, 0.9},
-			dungeon = {0,0,0, 0.9},
-			horde = {0,0,0, 0.9},
-			legendary = {0,0,0, 0.9},
-			legion = {0,0,0, 0.9},
-			recipe = {0,0,0, 0.9},
-			store = {0,0,0, 0.9},
-			transmog = {0,0,0, 0.9},
-			upgrade = {0,0,0, 0.9},
-			worldquest = {0,0,0, 0.9},
-		},
-})
+function LST:SkinToast(toast, toastType)
+	toast.Border:SetTexture([[Interface\AddOns\ls_Toasts_Rehok\media\toast-Border.tga]])
+	toast.TextBG:SetAlpha(1)
+	toast.BG:SetTexture([[Interface\AddOns\ls_Toasts_Rehok\media\normTex2.tga]]) -- This is from ElvUI
+	toast.BG:SetAlpha(1)
+	toast.BG:SetVertexColor(0.15,0.15,0.15)
+	toast.Icon:SetTexCoord(.08, .92, .08, .92)
+	toast.Icon:SetPoint("TOPLEFT", 5, -6)
+	toast.Icon:SetSize(47,47)
+	toast.IconBorder:SetTexture()
+	toast.IconText1BG:SetTexture()
+	toast.Title:SetFont([[Interface\AddOns\ls_Toasts_Rehok\media\Expressway.ttf]], 16)
+	toast.Title:SetPoint("TOPLEFT", 55, -12)
+
+	local r,g,b = toast.Border:GetVertexColor()
+
+	-- Fixes Currency icons
+	if (r >= 0.9) and (g >= 0.9) and (b >= 0.9) then
+		toast.Icon:SetSize(44,46)
+		toast.Icon:SetPoint("TOPLEFT", 6, -6)
+		toast.Border:SetTexture([[Interface\AddOns\ls_Toasts_Rehok\media\black-Border.TGA]])
+	end
+	-- Fixes Archaeology icon
+	if (r >= 0.9 and r < 0.92) and (g >= 0.39 and g < 0.41) and (b >= 0.1 and b < 0.11) then
+		toast.Icon:SetPoint("TOPLEFT", 7, -3)
+		toast.Icon:SetSize(76, 76)
+	end
+
+	-- Removes glow from Quest Items
+	if toast.IconHL then
+		toast.IconHL:SetTexture()
+	end
+
+	-- Removes Legendary Dragon Icon
+	if toast.Dragon then
+		toast.Dragon:SetTexture()
+	end
+
+end
