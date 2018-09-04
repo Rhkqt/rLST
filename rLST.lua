862acece8888bc5f5b79742e0adb4694ba9390df
Callum <cs139501@gmail.com>
2018-09-04 19:14:01 +0100

    Handle Azerite item background and new option to match my own personal UI that people might also like

--- rLST.lua
+++ rLST.lua
@@ -8,38 +8,40 @@ local LS = unpack(ls_Toasts)
 local background = {0,0,0,0.9}
 
 LS:RegisterSkin("rlst", {
-		name = "rLST",
-		template = "elv-no-art",
-		border = {
-			color = {0,0,0},
-			texture = {0, 0, 0, 1},
+	name = "rLST",
+	template = "elv",
+	border = {
+		color = {0,0,0},
+		texture = {0, 0, 0, 1},
+	},
+	icon_border = {
+		color = {0,0,0,0},
+	},
+	dragon = {
+		hidden = true,
+	  },
+	icon_highlight = {
+		hidden = true,
+	  },
+	bg = {
+		alliance = background,
+		archaeology = background,
+		azerite = background,
+		collection = background,
+		default = background,
+		dungeon = background,
+		horde = background,
+		legendary = background,
+		legion = background,
+		recipe = background,
+		store = background,
+		transmog = background,
+		upgrade = background,
+		worldquest = background,
 		},
-		icon_border = {
-			color = {0,0,0,0},
-		},
-		dragon = {
-		    hidden = true,
-		  },
-		icon_highlight = {
-		    hidden = true,
-		  },
-		bg = {
-				alliance = background,
-				archaeology = background,
-				collection = background,
-				default = background,
-				dungeon = background,
-				horde = background,
-				legendary = background,
-				legion = background,
-				recipe = background,
-				store = background,
-				transmog = background,
-				upgrade = background,
-				worldquest = background,
-			},
 })
- 
+
+
 LS:RegisterSkin("rlst-colour", {
 	name = "rLST (Colour)",
 	template = "elv-no-art",
@@ -55,6 +57,7 @@ LS:RegisterSkin("rlst-colour", {
 	bg = {
 			alliance = background,
 			archaeology = background,
+			azerite = background,
 			collection = background,
 			default = background,
 			dungeon = background,
@@ -66,5 +69,67 @@ LS:RegisterSkin("rlst-colour", {
 			transmog = background,
 			upgrade = background,
 			worldquest = background,
+		},
+})
+
+LS:RegisterSkin("rlst-opaque", {
+	name = "rLST (Opaque)",
+	template = "elv",
+	border = {
+		color = {0,0,0},
+		texture = {0, 0, 0, 1},
+	},
+	icon_border = {
+		color = {0,0,0,0},
+	},
+	dragon = {
+		hidden = true,
+	  },
+	icon_highlight = {
+		hidden = true,
+	  },
+	bg = {
+			alliance = {
+				texture = "Interface\\Addons\\rLST\\media\\background.tga",
+			},
+			archaeology = {
+				texture = "Interface\\Addons\\rLST\\media\\background.tga",
+			},
+			azerite  = {
+				texture = "Interface\\Addons\\rLST\\media\\background.tga",
+			},
+			collection = {
+				texture = "Interface\\Addons\\rLST\\media\\background.tga",
+			},
+			default = {
+				texture = "Interface\\Addons\\rLST\\media\\background.tga",
+			},
+			dungeon = {
+				texture = "Interface\\Addons\\rLST\\media\\background.tga",
+			},
+			horde = {
+				texture = "Interface\\Addons\\rLST\\media\\background.tga",
+			},
+			legendary = {
+				texture = "Interface\\Addons\\rLST\\media\\background.tga",
+			},
+			legion = {
+				texture = "Interface\\Addons\\rLST\\media\\background.tga",
+			},
+			recipe = {
+				texture = "Interface\\Addons\\rLST\\media\\background.tga",
+			},
+			store = {
+				texture = "Interface\\Addons\\rLST\\media\\background.tga",
+			},
+			transmog = {
+				texture = "Interface\\Addons\\rLST\\media\\background.tga",
+			},
+			upgrade = {
+				texture = "Interface\\Addons\\rLST\\media\\background.tga",
+			},
+			worldquest = {
+				texture = "Interface\\Addons\\rLST\\media\\background.tga",
+			},
 		},
 })
